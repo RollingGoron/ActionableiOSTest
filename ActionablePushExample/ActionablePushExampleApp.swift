67ac0d45164960f2b4bb388bf1af77445cdb9740
Peter Gosling <p.gosling@icloud.com>
2022-07-19 10:58:13 -0700

    Initial Commit

---
+++ ActionablePushExample/ActionablePushExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActionablePushExampleApp.swift
+//  ActionablePushExample
+//
+//  Created by Peter Gosling on 7/19/22.
+//
+
+import SwiftUI
+
+@main
+struct ActionablePushExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
